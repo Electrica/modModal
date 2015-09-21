7ba6a4dee532dbeb7f62a77b31774b4a6f497184
Electrica <electricaev@gmail.com>
2015-09-21 15:50:41 +0600

    Добавили параметр показа по времени.

--- core/components/modmodal/elements/plugins/plugin.modmodal.php
+++ core/components/modmodal/elements/plugins/plugin.modmodal.php
@@ -1,30 +1,34 @@
 <?php
 
-switch ($modx->event->name) {
+$id = $modx->resourceIdentifier;
+$delay = $modx->getOption('modmodal_delay') ? $modx->getOption('modmodal_delay') : 5000;
+$event = $modx->event->name;
 
-    case('OnWebPagePrerender'):
+if($event == 'OnWebPageInit' || $event == 'OnWebPagePrerender'){
 
+    if($modx->getOption('modmodal_active')){
 
-        if($modx->getOption('modmodal_active')){
-
-            $id = $modx->resourceIdentifier;
-
+        if($modx->getOption('modmodal_id') == $id){
             if($modx->getOption('modmodal_include_jquery')){
                 $modx->regClientScript($modx->getOption('assets_url') . 'components/modmodal/js/web/jquery-1.11.3.min.js');
             }
+
+
+
             $modx->regClientCSS($modx->getOption('assets_url') . 'components/modmodal/css/web/jquery.arcticmodal-0.3.css');
+            $modx->regClientCSS($modx->getOption('modmodal_themes'));
+            $modx->regClientScript($modx->getOption('assets_url') . 'components/modmodal/js/web/jquery.arcticmodal-0.3.min.js');
+
+            $main_js = file_get_contents($modx->getOption('assets_path') . 'components/modmodal/js/web/main.js');
+            $main_js = str_replace('delay_time', $delay, $main_js);
+            $modx->regClientHTMLBlock('<script>' . $main_js . '</script>');
 
             $html = $modx->getChunk($modx->getOption('modmodal_chunk'));
 
             if(strpos($modx->resource->_output, '</body>') !== false){
                 $modx->resource->_output = preg_replace("/(<\/body>)/i", $html . "\n\\1", $modx->resource->_output, true);
             }
-
         }
-
-        break;
-
-    default:
-        break;
+    }
 
 }
